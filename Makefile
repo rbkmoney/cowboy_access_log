2349d5105911b3295df57839f4769f03c08f8c4f
Dmitry Manik <d.manik@rbkmoney.com>
2017-12-04 15:34:32 +0300

    MSPF-320: Introduce access lib

---
+++ Makefile
@@ -0,0 +1,49 @@
+REBAR := $(shell which rebar3 2>/dev/null || which ./rebar3)
+SUBMODULES = build_utils
+SUBTARGETS = $(patsubst %,%/.git,$(SUBMODULES))
+
+UTILS_PATH := build_utils
+# ToDo: remove unused TEMPLATES_PATH here, when the bug
+# with handling of the varriable in build_utils is fixed
+TEMPLATES_PATH := .
+SERVICE_NAME := scoper
+BUILD_IMAGE_TAG := 4fa802d2f534208b9dc2ae203e2a5f07affbf385
+
+CALL_W_CONTAINER := all submodules rebar-update compile xref lint dialyze test clean distclean
+
+.PHONY: $(CALL_W_CONTAINER)
+
+all: compile
+
+-include $(UTILS_PATH)/make_lib/utils_container.mk
+
+$(SUBTARGETS): %/.git: %
+	git submodule update --init $<
+	touch $@
+
+submodules: $(SUBTARGETS)
+
+compile: submodules rebar-update
+	$(REBAR) compile
+
+rebar-update:
+	$(REBAR) update
+
+test: submodules
+	$(REBAR) ct
+
+xref: submodules
+	$(REBAR) as test xref
+
+clean:
+	$(REBAR) clean
+
+distclean:
+	$(REBAR) clean -a
+	rm -rfv _build _builds _cache _steps _temp
+
+dialyze:
+	$(REBAR) as test dialyzer
+
+lint:
+	elvis rock
---
+++ src/access_lib.erl
@@ -0,0 +1,83 @@
+-module(access_lib).
+
+%% API exports
+-export([log_access/4]).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+-spec log_access(atom(), pos_integer(), list(), cowboy_req:req()) ->
+    ok | {error, lager_not_running}.
+log_access(SinkName, Code, Headers, Req) ->
+    {Method, _} = cowboy_req:method(Req),
+    {Path,   _} = cowboy_req:path(Req),
+    {ReqLen, _} = cowboy_req:body_length(Req),
+    {ReqId,  _} = cowboy_req:header(<<"x-request-id">>, Req, undefined),
+    RemAddr  = get_remote_addr(Req),
+    RespLen  = get_response_len(Headers),
+    Duration = get_request_duration(Req),
+    ReqMeta = [
+               {remote_addr, RemAddr},
+               {request_method, Method},
+               {request_path, Path},
+               {request_length, ReqLen},
+               {response_length, RespLen},
+               {request_time, Duration},
+               {'http_x-request-id', ReqId},
+               {status, Code}
+              ],
+    Meta = orddict:merge(fun(_Key, New, _Old) -> New end, ReqMeta, lager:md()),
+    %% Call lager:log/5 here directly in order to pass request metadata (fused into
+    %% lager metadata) without storing it in a process dict via lager:md/1.
+    lager:log(SinkName, info, Meta, "", []).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+-define(START_TIME_TAG, processing_start_time).
+
+get_remote_addr(Req) ->
+    case determine_peer(Req) of
+        {{ok, #{ip_address := IP}}, _} ->
+            genlib:to_binary(inet:ntoa(IP));
+        {_, _} ->
+            undefined
+    end.
+
+determine_peer(Req) ->
+    {Peer, Req1}  = cowboy_req:peer(Req),
+    {Value, Req2} = cowboy_req:header(<<"x-forwarded-for">>, Req1),
+    {determine_peer_from_header(Value, Peer), Req2}.
+
+determine_peer_from_header(undefined, {IP, Port}) ->
+    % undefined, assuming no proxies were involved
+    {ok, #{ip_address => IP, port_number => Port}};
+determine_peer_from_header(Value, _Peer) when is_binary(Value) ->
+    ClientPeer = string:strip(binary_to_list(Value)),
+    case string:tokens(ClientPeer, ", ") of
+        [ClientIP | _Proxies] ->
+            case inet:parse_strict_address(ClientIP) of
+                {ok, IP} ->
+                    {ok, #{ip_address => IP}};
+                Error ->
+                    Error
+            end;
+        _ ->
+            {error, malformed}
+    end.
+
+get_request_duration(Req) ->
+    case cowboy_req:meta(?START_TIME_TAG, Req) of
+        {undefined, _} ->
+            undefined;
+        {StartTime, _} ->
+            (genlib_time:ticks() - StartTime) / 1000000
+    end.
+
+get_response_len(Headers) ->
+    case lists:keyfind(<<"content-length">>, 1, Headers) of
+        {_, Len} ->
+            genlib:to_int(Len);
+        false ->
+            undefined
+    end.
