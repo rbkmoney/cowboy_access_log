eab88e2e46d32d1ec281c0f5618bac8a27a132e2
Dmitry Manik <d.manik@rbkmoney.com>
2017-12-05 14:13:13 +0300

    MSPF-320: Fix review fixes

--- src/cowboy_access_log.erl
+++ src/cowboy_access_log.erl
@@ -1,26 +1,55 @@
 -module(cowboy_access_log).
 
 %% API exports
--export([log_access/4]).
--export([set_meta  /1]).
+-export([request_hook /1]).
+-export([response_hook/5]).
 
--define(START_TIME_TAG, {?MODULE, request_handling_started_at}).
+-define(START_TIME_TAG, cowboy_access_log_request_handling_started_at).
 
 %%====================================================================
 %% API functions
 %%====================================================================
--spec log_access(atom(), pos_integer(), list(), cowboy_req:req()) ->
-    ok | {error, lager_not_running}.
+-spec request_hook(cowboy_req:req()) ->
+    cowboy_req:req().
+
+request_hook(Req) ->
+    set_meta(Req).
+
+-spec response_hook(atom(), cowboy:http_status(), cowboy:http_headers(), iodata(), cowboy_req:req()) ->
+    cowboy_req:req().
+
+response_hook(SinkName, Code, Headers, _, Req) ->
+    try
+        {Code1, Headers1, Req1} = handle_response(Code, Headers, Req),
+        _ = log_access(SinkName, Code1, Headers1, Req1),
+        Req1
+    catch
+        Class:Reason ->
+            Stack = genlib_format:format_stacktrace(erlang:get_stacktrace(), [newlines]),
+            _ = lager:error(
+                    "Response hook failed for: [~p, ~p, ~p]~nwith: ~p:~p~nstacktrace: ~ts",
+                    [Code, Headers, Req, Class, Reason, Stack]
+                ),
+            Req
+    end.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+handle_response(Code, Headers, Req) ->
+    {Code, Headers, Req}.
+
 log_access(SinkName, Code, Headers, Req) ->
     {Method, _} = cowboy_req:method(Req),
     {Path,   _} = cowboy_req:path(Req),
     {ReqLen, _} = cowboy_req:body_length(Req),
     {ReqId,  _} = cowboy_req:header(<<"x-request-id">>, Req, undefined),
-    RemAddr  = get_remote_addr(Req),
+    {RemAddr, PeerAddr} = get_remote_addr(Req),
     RespLen  = get_response_len(Headers),
     Duration = get_request_duration(Req),
     ReqMeta = [
-        {remote_addr, RemAddr},
+        {rem_addr, RemAddr},
+        {peer_addr, PeerAddr},
         {request_method, Method},
         {request_path, Path},
         {request_length, ReqLen},
@@ -39,13 +68,10 @@ log_access(SinkName, Code, Headers, Req) ->
 set_meta(Req) ->
     cowboy_req:set_meta(?START_TIME_TAG, genlib_time:ticks(), Req).
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
 get_remote_addr(Req) ->
     case determine_peer(Req) of
-        {{ok, #{ip_address := IP}}, _} ->
-            genlib:to_binary(inet:ntoa(IP));
+        {{ok, #{rem_addr := RemAddr, peer_addr := PeerAddr}}, _} ->
+            {genlib:to_binary(inet:ntoa(RemAddr)), genlib:to_binary(inet:ntoa(PeerAddr))};
         {_, _} ->
             undefined
     end.
@@ -55,16 +81,16 @@ determine_peer(Req) ->
     {Value, Req2} = cowboy_req:header(<<"x-forwarded-for">>, Req1),
     {determine_peer_from_header(Value, Peer), Req2}.
 
-determine_peer_from_header(undefined, {IP, Port}) ->
+determine_peer_from_header(undefined, {IP, _Port}) ->
     % undefined, assuming no proxies were involved
-    {ok, #{ip_address => IP, port_number => Port}};
-determine_peer_from_header(Value, _Peer) when is_binary(Value) ->
+    {ok, #{rem_addr => IP, peer_addr => IP}};
+determine_peer_from_header(Value, {PeerIP, _Port}) when is_binary(Value) ->
     ClientPeer = string:strip(binary_to_list(Value)),
     case string:tokens(ClientPeer, ", ") of
         [ClientIP | _Proxies] ->
             case inet:parse_strict_address(ClientIP) of
                 {ok, IP} ->
-                    {ok, #{ip_address => IP}};
+                    {ok, #{rem_addr => IP, peer_addr => PeerIP}};
                 Error ->
                     Error
             end;
