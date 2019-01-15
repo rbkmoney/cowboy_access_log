5fb99f930bf61ddb401e5155b47f67cc68b2fd2c
4tyTwo <i.toporkov@rbkmoney.com>
2019-01-15 11:30:57 +0300

    Moved metadata to stream state

--- src/cowboy_access_log_h.erl
+++ src/cowboy_access_log_h.erl
@@ -13,22 +13,22 @@
 -export([terminate/3]).
 -export([early_error/5]).
 
+-define(START_TIME_TAG, cowboy_access_log_request_handling_started_at).
+
 -type state() :: #{
     next := any(),
     req  := cowboy_req:req(),
-    sink := atom()
+    sink := atom(),
+    meta := #{?START_TIME_TAG => genlib_time:ticks()}
 }.
-
--define(START_TIME_TAG, cowboy_access_log_request_handling_started_at).
 
 %% callbacks
 
 -spec init(cowboy_stream:streamid(), cowboy_req:req(), cowboy:opts())
     -> {cowboy_stream:commands(), state()}.
-init(StreamID, Req0, Opts) ->
-    Req = set_meta(Req0),
+init(StreamID, Req, Opts) ->
     {Commands0, Next} = cowboy_stream:init(StreamID, Req, Opts),
-    {Commands0, #{next => Next, sink => get_sink(Opts), req => Req}}.
+    {Commands0, set_meta(#{next => Next, sink => get_sink(Opts), req => Req})}.
 
 -spec data(cowboy_stream:streamid(), cowboy_stream:fin(), cowboy_req:resp_body(), State)
     -> {cowboy_stream:commands(), State} when State::state().
@@ -39,7 +39,7 @@ data(StreamID, IsFin, Data, #{next := Next0} = State) ->
 -spec info(cowboy_stream:streamid(), any(), State)
     -> {cowboy_stream:commands(), State} when State::state().
 info(StreamID, {response, Code, Headers, _} = Info, #{next := Next0, sink := SinkName, req := Req} = State) ->
-    _ = log_access_safe(SinkName, Code, Headers, Req),
+    _ = log_access_safe(SinkName, Code, Headers, Req, State),
     {Commands0, Next} = cowboy_stream:info(StreamID, Info, Next0),
     {Commands0, State#{next => Next}};
 info(StreamID, Info, #{next := Next0} = State) ->
@@ -53,16 +53,15 @@ terminate(StreamID, Reason, #{next := Next}) ->
 -spec early_error(cowboy_stream:streamid(), cowboy_stream:reason(),
     cowboy_stream:partial_req(), Resp, cowboy:opts()) -> Resp
     when Resp::cowboy_stream:resp_command().
-early_error(StreamID, Reason, PartialReq0, {_, Code, Headers, _} = Resp, Opts) ->
-    PartialReq = set_meta(PartialReq0),
-    _ = log_access_safe(get_sink(Opts), Code, Headers, PartialReq),
+early_error(StreamID, Reason, PartialReq, {_, Code, Headers, _} = Resp, Opts) ->
+    _ = log_access_safe(get_sink(Opts), Code, Headers, PartialReq, set_meta(Opts)),
     cowboy_stream:early_error(StreamID, Reason, PartialReq, Resp, Opts).
 
 %% private functions
 
-log_access_safe(SinkName, Code, Headers, Req) ->
+log_access_safe(SinkName, Code, Headers, Req, Opts) ->
     try
-        _ = log_access(SinkName, Code, Headers, Req),
+        _ = log_access(SinkName, Code, Headers, Req, Opts),
         Req
     catch
         Class:Reason:Stacktrace ->
@@ -74,19 +73,19 @@ log_access_safe(SinkName, Code, Headers, Req) ->
             Req
     end.
 
-log_access(SinkName, Code, Headers, Req) ->
+log_access(SinkName, Code, Headers, Req, Opts) ->
     %% Call lager:log/5 here directly in order to pass request metadata (fused into
     %% lager metadata) without storing it in a process dict via lager:md/1.
-    lager:log(SinkName, info, prepare_meta(Code, Headers, Req), "", []).
+    lager:log(SinkName, info, prepare_meta(Code, Headers, Req, Opts), "", []).
 
-prepare_meta(Code, Headers, Req) ->
+prepare_meta(Code, Headers, Req, Opts) ->
     MD1 = set_log_meta(remote_addr, get_remote_addr(Req), lager:md()),
     MD2 = set_log_meta(peer_addr, get_peer_addr(Req), MD1),
     MD3 = set_log_meta(request_method, cowboy_req:method(Req), MD2),
     MD4 = set_log_meta(request_path, cowboy_req:path(Req), MD3),
     MD5 = set_log_meta(request_length, cowboy_req:body_length(Req), MD4),
     MD6 = set_log_meta(response_length, get_response_len(Headers), MD5),
-    MD7 = set_log_meta(request_time, get_request_duration(Req), MD6),
+    MD7 = set_log_meta(request_time, get_request_duration(Opts), MD6),
     MD8 = set_log_meta('http_x-request-id', cowboy_req:header(<<"x-request-id">>, Req, undefined), MD7),
     set_log_meta(status, Code, MD8).
 
@@ -130,8 +129,8 @@ determine_remote_addr_from_header(Value, _Peer) when is_binary(Value) ->
 determine_remote_addr_from_header(undefined, undefined) ->
     {error, undefined}.
 
-get_request_duration(Req) ->
-    case maps:get(meta, Req, undefined) of
+get_request_duration(Opts) ->
+    case maps:get(meta, Opts, undefined) of
         undefined ->
             undefined;
         Meta ->
@@ -145,14 +144,14 @@ get_request_duration(Req) ->
 
 get_response_len(Headers) ->
     case maps:get(<<"content-length">>, Headers, undefined) of
-         undefined ->
+        undefined ->
             undefined;
         Len ->
             genlib:to_int(Len)
     end.
 
-set_meta(Req) ->
-    maps:put(meta, #{?START_TIME_TAG => genlib_time:ticks()}, Req).
+set_meta(Opts) ->
+    Opts#{meta => #{?START_TIME_TAG => genlib_time:ticks()}}.
 
 get_sink(#{env := Env}) ->
     maps:get(sink, Env).
@@ -164,7 +163,7 @@ get_sink(#{env := Env}) ->
 
 -spec filter_meta_test() -> _.
 filter_meta_test() ->
-    Req0 = #{
+    Req = #{
         pid => self(),
         peer => undefined,
         method => <<"GET">>,
@@ -177,14 +176,14 @@ filter_meta_test() ->
         has_body => false,
         body_length => 0
     },
-    Req = set_meta(Req0), 
-   [
+    State = set_meta(#{}),
+    [
         {request_length, 0},
         {request_method, <<"GET">>},
         {request_path, <<>>},
         {request_time, _},
         {response_length, 33},
         {status, 200}
-    ] = prepare_meta(200, #{<<"content-length">> => <<"33">>}, Req).
+    ] = prepare_meta(200, #{<<"content-length">> => <<"33">>}, Req, State).
 
 -endif.
